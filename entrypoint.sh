2d59549a6ee33d59f6a85df73b47eacfe1f6e545
Datagate <datagate_service@example.com>
2022-08-25 15:34:01 +0800

    master

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,17 @@ export git_source_key=$2
 export git_remote=$3
 export git_remote_key=$4
 
+echo 0 > /tmp/git-sync-returncode.txt
+
 set -o pipefail; /sync.sh  2>&1 | tee /tmp/git-sync-aciton.log || echo $? > /tmp/git-sync-returncode.txt
 
 export sync_code=`cat /tmp/git-sync-returncode.txt`
 unix2dos /tmp/git-sync-aciton.log
 export sync_log=`cat /tmp/git-sync-aciton.log` 
+
+sync_log="${sync_log//'%'/'%25'}"
+sync_log="${sync_log//$'\n'/'%0A'}"
+sync_log="${sync_log//$'\r'/'%0D'}"
 
 echo "::set-output name=sync_code::$sync_code"
 echo "::set-output name=sync_log::$sync_log"
